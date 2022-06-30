563950aa73d57e9c3ecd27ccf399f6ed4375cf29
is0405 <Junknhinamyzk@outlook.jp>
2022-06-30 11:47:18 +0900

    first commit

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	server "github.com/is0405/docker-env"
+)
+
+func main() {
+	var databaseDatasource string
+	var port int
+
+	flag.StringVar(&databaseDatasource, "databaseDatasource", "root:password@tcp(localhost:3306)/dbapp", "Should looks like root:password@tcp(hostname:port)/dbname")
+	flag.IntVar(&port, "port", 1991, "Web server port")
+	flag.Parse()
+
+	log.SetFlags(log.Ldate + log.Ltime + log.Lshortfile)
+	log.SetOutput(os.Stdout)
+
+	s := server.NewServer()
+	if err := s.Init(databaseDatasource); err != nil {
+		log.Fatal(err)
+	}
+	s.Run(port)
+}
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/is0405/docker-env
+
+go 1.18
