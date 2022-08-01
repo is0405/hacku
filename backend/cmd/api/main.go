8635f4db115d78be8c9f476ca327a8194232747c
is0405 <Junknhinamyzk@outlook.jp>
2022-08-01 15:32:34 +0900

    データベースの接続問題解消

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -12,8 +12,8 @@ func main() {
 	var databaseDatasource string
 	var port int
 
-	flag.StringVar(&databaseDatasource, "databaseDatasource", "root:password@tcp(localhost:3306)/hacku", "Should looks like root:password@tcp(hostname:port)/dbname")
-	flag.IntVar(&port, "port", 1991, "Web server port")
+	flag.StringVar(&databaseDatasource, "databaseDatasource", "root:password@tcp(db:3306)/hacku", "Should looks like root:password@tcp(hostname:port)/dbname")
+	flag.IntVar(&port, "port", 10001, "Web server port")
 	flag.Parse()
 
 	log.SetFlags(log.Ldate + log.Ltime + log.Lshortfile)
