ad45c14b9aae4943b363c8d2690c8690eb2fc58e
is0405 <Junknhinamyzk@outlook.jp>
2022-08-12 21:47:25 +0900

    仮ID追加

--- database/migration/schema/V2__add_user.sql
+++ database/migration/schema/V2__add_user.sql
@@ -1,2 +1,5 @@
 INSERT INTO `sub_user` (name, mail, age, faculty, password, gender,code)
 VALUES ("kawano", "is04@ed.jp", 22, 2, "88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589", 2, "123456");
+
+INSERT INTO `user` (name, mail, age, faculty, password, gender)
+VALUES ("hacu", "hacu@ac.jp", 20, 2, "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 2);
